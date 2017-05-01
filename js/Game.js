c41b9714384b819395db755d39463260ee8ac415
NoahJohnson49 <mnwild9.64@gmail.com>
2017-04-30 23:41:30 -0500

    Add files via upload

--- js/Game.js
+++ js/Game.js
@@ -15,12 +15,8 @@ var Start_Container;
 
 var ticks = 0;
 
-var speedLbl;
-var generLbl;
-
-var stuff;
-
 function game_init(){
+	//Starts game with game building functions and switches game state
 	Game_State = GAME.none;
 	Game_initStage();
 	Game_buildStage();
@@ -28,113 +24,97 @@ function game_init(){
 	init_AsteroidGenerator();
 	Asteroid_Generator.start();
 	Asteroid_Generator.generate();
-
-	speedLbl = document.getElementById('speedLbl');
-	generLbl = document.getElementById('generLbl');
-
 	//Game_pauseWithoutOverlay();
 	Game_showStartOverlay();
 }
 
 function Game_showStartOverlay(){
+	//Creating background for start screen overlay with rectangle shape
 	var background = new createjs.Shape();
 	background.graphics.beginFill('#666').drawRect(0,0,400,90);
 	background.x = 150;
 	background.y = 240;
 	background.alpha = 0.8;
-
+	//Writing text for overlay over the rectangle shape
 	var text = new createjs.Text("Press 'Enter' to start.", "30px Arial", "BBB");
 	text.x = 350;
 	text.y = 270;
 	text.textAlign = 'center';
-
+	//Creating container to hold shape and text element
 	Start_Container = new createjs.Container();
 	Start_Container.addChild(background);
 	Start_Container.addChild(text);
-
+	//adding container to stage and changing game state
 	stage.addChild(Start_Container);
 	stage.update();
 	Game_State = GAME.starting
 }
 
 function Game_createStatBox(){
+	//Creating stat box with rectangle shape
 	var cc = new createjs.Shape();
 	cc.graphics.beginFill('#666').drawRect(0, 0, 90, 70);
 	cc.x = 10;
 	cc.y = 10;
 	cc.alpha = 0.8;
-
+	//Creating text object that updates for stat box
 	scoreText = new createjs.Text("Score : " + Score_Object.getCurrentScore(), "15px Arial", "#fff");
 	scoreText.x = 15;
 	scoreText.y = 15;
-
 	livesText = new createjs.Text("Lives : " + Player_Object.getLives(), "15px Arial", "#fff");
 	livesText.x = 15;
 	livesText.y = 55;
-
+	//creating container to hold text and rectangle object
 	var contain = new createjs.Container();
 	contain.addChild(cc);
 	contain.addChild(scoreText);
 	contain.addChild(livesText);
-
+	//Adding container to stage
 	stage.addChild(contain);
 	stage.update();
-	//setTimeout(function(){ scoreText.text = "Score : " + Score_Object.addScore(); stage.update();}, 2000);
-	//setTimeout(function(){ scoreText.text = "Score : " + Score_Object.addScore(); stage.update();}, 4000);
-	//setTimeout(function(){ scoreText.text = "Score : " + Score_Object.addScore(); stage.update();}, 6000);
-	//setTimeout(function(){ Player_Object.changeLife(1); livesText.text = "Lives : " + Player_Object.getLives(); stage.update();}, 3000);
-	//setTimeout(function(){ Player_Object.changeLife(1); livesText.text = "Lives : " + Player_Object.getLives(); stage.update();}, 5000);
-	//setTimeout(function(){Player_Object.died();}, 2000);
-	//setTimeout(function(){Player_Object.died();}, 4000);
-	//setTimeout(function(){Player_Object.died();}, 6000);
-	//setTimeout(function(){Player_Object.died();}, 8000);
-}
 
 function Game_hideStartOverlay(){
+	//Hides the start overlay and removes from the stage
 	stage.removeChild(Start_Container);
 	stage.update();
 }
 
 function Game_start(){
+	//Changes game state and hides the start overlay
 	Game_State = GAME.none;
 	Game_hideStartOverlay();
-
+	//Removes all the asteroids for clean slate
 	Asteroid_List.removeAll();
-
+	//Starts asteroid generator
 	Asteroid_Generator.timeUntilNextAsteroid = 500;
 	Asteroid_Generator.speed = 2;
-
+	//Creates player object 
 	Player_Object = new_Player( 345, 295);
 	stage.addChild(Player_Object);
 	stage.update();
-
+	//Creates score object for player
 	Score_Object = new_Score();
-
+	//Creates stat box associated to Players lives and points
 	Game_createStatBox();
-
+	//Changes pause setting and game state
 	Game_unpause();
 	Game_State = GAME.running;
 }
 
-function Game_reset(){
-
-}
-
 function Game_end(){
-	Game_State = GAME.none;
-
+	//Switches game state to end game
 	Game_State = GAME.ended;
-
+	//Creates game over box rectangle
 	var gameOverBox = new createjs.Shape();
 	gameOverBox.graphics.beginFill("#666").drawRect(0, 0, 400, 90);
 	gameOverBox.x = 150;
 	gameOverBox.y = 240;
 	gameOverBox.alpha = 0.8;
-
-	var gameover = new createjs.Text("Game Over", "30px Arial", "#BBB");
-	gameover.x = 270;
-	gameover.y = 250;
-
+	//Creates game over text 
+	var gameover = new createjs.Text("Game Over", "50px Arial", "#fff");
+	gameover.x = 220;
+	gameover.y = 260;
+	//Creates container to hold text and rectangle and adds to stage
 	var gameOverContainer = new createjs.Container();
 	gameOverContainer.addChild(gameOverBox);
 	gameOverContainer.addChild(gameover);
@@ -143,39 +123,44 @@ function Game_end(){
 }
 
 function Game_initStage(){
+	//Starts stage and builds ticker
 	Game_buildStage()
 	createjs.Ticker.setFPS(60);
+	//Sets up both lists and event listener 
 	Asteroid_List = new_AsteroidList();
 	Bullet_List = new_BulletList();
+	//event listener for tick to update the game
 	createjs.Ticker.addEventListener('tick',Game_update);
 }
 
 function Game_buildStage(){
+	//Build stage with canvas from HTML and create Pause container
 	stage = new createjs.Stage('canvas');
 	Paused_Container = new createjs.Container();
-
+	//Build background
 	var background = new createjs.Shape();
 	background.graphics.beginFill('#666').drawRect(0,0,300,120);
 	background.x = 200;
 	background.y = 240;
 	background.alpha = 0.8;
-
+	//Build text for Background of paused container
 	var text1 = new createjs.Text("Paused", "35px Arial", "#EEE");
 	text1.x = 350;
 	text1.y = 260;
 	text1.textAlign = 'center';
-
+	//Second text for how to unpause
 	var text2 = new createjs.Text("Press 'Enter' to continue.", "22px Arial", "BBB");
 	text2.x = 350;
 	text2.y = 310;
 	text2.textAlign = 'center';
-
+	//Adds children to pause box
 	Paused_Container.addChild(background);
 	Paused_Container.addChild(text1);
 	Paused_Container.addChild(text2);
 }
 
 function Game_setControls(){
+	//Setting basic control input with key codes
 	window.onkeydown = handleKeyDown;
 	window.onkeyup = handleKeyUp;
 	var KEY = { 
@@ -190,6 +175,7 @@ function Game_setControls(){
 	}
 
 	function handleKeyDown(e){
+		//Checks wich key was pushed and handles key down functionality
 		switch(e.keyCode){
 			case KEY.right_arrow :
 				Player_Object.rotate = MOVE.right;
@@ -243,6 +229,7 @@ function Game_setControls(){
 	}
 
 	function handleKeyUp(e){
+		//Handles key up from initial key down 
 		switch(e.keyCode){
 			case KEY.right_arrow :
 				if(Player_Object.rotate == MOVE.right)
@@ -282,9 +269,8 @@ function Game_setControls(){
 }
 
 function Game_update(){
-
-	stuff = Asteroid_List.getAll();
-
+	//Updates Game. 
+	//We use the amount of ticks to determine what to check because we don't have to constantly be running the updates for both lists
 	if(!createjs.Ticker.paused){
 		ticks++
 		if(Asteroid_List != null){
@@ -304,7 +290,6 @@ function Game_update(){
 		if(!document.hasFocus() && Game_State == GAME.running){
 			Game_pauseWithOverlay();
 		}
-
 		//This makes the game harder.
 		if(ticks == 2400){
 			Asteroid_Generator.timeUntilNextAsteroid = LEVEL.two[1];
@@ -322,21 +307,23 @@ function Game_update(){
 	}else{
 
 	}
-
 	stage.update();
 }
 
 function Game_pauseWithOverlay(){
+	//Pauses game with overlay
 	Game_State = GAME.none;
 	createjs.Ticker.paused = true;
 	Asteroid_Generator.stop();
-
+	//Adds pause overlay to stage and updates
 	stage.addChild(Paused_Container);
 	stage.update();
+	//Updates game state to paused
 	Game_State = GAME.paused;
 }
 
 function Game_pauseWithoutOverlay(){
+	//Pauses ticker, generator, and the game state
 	Game_State = GAME.none;
 	createjs.Ticker.paused = true;
 	Asteroid_Generator.stop();
@@ -344,65 +331,23 @@ function Game_pauseWithoutOverlay(){
 }
 
 function Game_unpause(){
+	//unpauses the state, ticker, and generator
 	Game_State = GAME.none;
 	createjs.Ticker.paused = false;
 	Asteroid_Generator.start();
-
+	//removes pause container
 	stage.removeChild(Paused_Container);
 	stage.update();
-
+	//Game state switch
 	Game_State = GAME.running;
 }
 
 function Game_deleteAllObjects(){
+	//Deletes all asteroids from list
 	Asteroid_List.removeAll();
 }
 
-function button1(e){
-	if(createjs.Ticker.paused)
-		Game_unpause();
-	else
-		Game_pauseWithOverlay();
-}
-
-function button2(e){
-	if(createjs.Ticker.paused)
-		Game_unpause();
-	else
-		Game_pauseWithoutOverlay();
-}
-
-
-
-function button3(e){
-	Asteroid_Generator.stop();
-}
-
-function button4(e){
-	Asteroid_Generator.start();
-}
-
-function button5(e){
-	Asteroid_List.removeAll();
-}
-function button6(e){
-	Asteroid_Generator.speed += .25;
-	speedLbl.innerHTML = "Speed : " + Asteroid_Generator.speed;
-}
-function button7(e){
-	if(Asteroid_Generator.timeUntilNextAsteroid > 51)
-		Asteroid_Generator.timeUntilNextAsteroid -= 50;
-	generLbl.innerHTML = "Generation Delay : " + Asteroid_Generator.timeUntilNextAsteroid;
-}
-function button8(e){
-	Asteroid_Generator.timeUntilNextAsteroid += 50;
-	generLbl.innerHTML = "Generation Delay : " + Asteroid_Generator.timeUntilNextAsteroid;
-}
-function button9(e){
-	Asteroid_Generator.speed -= .25;
-	speedLbl.innerHTML = "Speed : " + Asteroid_Generator.speed;
-}
-
+//Below are the different Game States for the game state machine
 var GAME = {
 	starting : 0,
 	paused : 1,
@@ -412,6 +357,7 @@ var GAME = {
 }
 
 function updateLivesText(){
+	//function that updates the text element of Lives in the stats box
 	var x = livesText.text;
 	if(x === Player_Object.getLivesString()){
 
@@ -422,6 +368,7 @@ function updateLivesText(){
 }
 
 function updateScoreText(){
+	//function that updates the text element of Score in the stats box
 	var x = scoreText.text;
 	if( x === Score_Object.getCurrentScoreString()){
 
@@ -431,6 +378,8 @@ function updateScoreText(){
 	}
 }
 
+
+//Below are the different level stats (Asteroid speed mainly)
 var LEVEL = {
 	one : [2,500],
 	two : [2,450],
